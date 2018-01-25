6b1ec10008f1c108002d22bc631137079fae0af1
smartDataDev <marcin.kalat@gmail.com>
2018-01-25 16:46:33 +0000

    simple hello program

---
+++ hello.py
@@ -0,0 +1,17 @@
+# This is my first program
+
+print ( 'Hi there')
+print ( 'What is your name ? ' ) # Ask for your name
+myName = input ()
+print ( 'It is good to meet you, ' + myName )
+print ( 'the lenght of your name is: ')
+print (len(myName))
+print ( 'What is your age ' + myName + ' ?') # Ask for your age
+myAge = input()
+print ( 'You will be ' + str(int(myAge) + 1) + ' next year.')
+print ( 'What is your favorite meal ' + myName + ' ?')
+myMeal = input()
+print ( myMeal + ' is my favourite meal, too !!!!!')
+
+
+        
