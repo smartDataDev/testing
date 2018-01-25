d0c82afe5d367e3299677274d710cf1931f27137
smartDataDev <35808284+smartDataDev@users.noreply.github.com>
2018-01-25 18:24:15 +0000

    some bugs corrected

--- first_game.py
+++ first_game.py
@@ -6,9 +6,9 @@ import random
 x = random.randint(10, 20)
 if x == 15:
     print ('You are winner !')
-elif x < 14:
+elif x <= 14:
     print ('Sorry, you are below the target. Try again ...')
-elif x > 16:
+elif x >= 16:
     print ('Sorry, you are above the target. Try again ...')
 
 else:
