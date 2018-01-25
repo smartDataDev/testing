4696493333a2947997e48b86e793e8972e249309
smartDataDev <marcin.kalat@gmail.com>
2018-01-25 18:12:19 +0000

    simple python game

---
+++ first_game.py
@@ -0,0 +1,23 @@
+# random/sys example
+
+import random
+
+
+x = random.randint(10, 20)
+if x == 15:
+    print ('You are winner !')
+elif x < 14:
+    print ('Sorry, you are below the target. Try again ...')
+elif x > 16:
+    print ('Sorry, you are above the target. Try again ...')
+
+else:
+    print ('You almost there. Please try again ... ')
+
+    
+'''
+import sys
+print ('Hello')
+sys.exit()
+print ('You will not see it') 
+'''
